f4441fb43f7eb98647a067186854003abbb1c479
Fabian Montes <fabianmontes11fab@gmail.com>
2024-02-05 23:16:46 -0600

    feat: leer intento

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-let intentos = 6;
+const limiteIntentos = 6;
+let intento = 1;
 let pistas = 2;
 let letras = 6;
 let palabraSecreta = generarPalabraAleatoria();
 console.log(palabraSecreta);
 
+function leerIntentoActual() {
+    palabra = '';
+    for(let i=1; i<=letras; i++) {
+        palabra += document.getElementById(`intento${intento}-letra${i}`).value.toUpperCase();
+    }
+    return palabra;
+}
+function adivinar() {
+    let palabraUsuario = leerIntentoActual();
+    console.log(palabraUsuario);
+}
 function generarPalabraAleatoria() {
     const banco = '"ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let palabra = '';
@@ -15,7 +27,7 @@ function generarPalabraAleatoria() {
 function crearEntradas() {
     let area = document.getElementById('entrada');
     area.innerHTML = '';
-    for(let i=1; i<=intentos; i++) {
+    for(let i=1; i<=limiteIntentos; i++) {
         fila = `<div id="intento${i}"> <span>intento #${i}<span>`;
         for(let j=1; j<=letras; j++) {
             fila += `<input class="casilla" id="intento${i}-letra${j}" maxlength="1" disabled>`;
@@ -26,7 +38,7 @@ function crearEntradas() {
     habilitarEntrada(1);
 }
 function habilitarEntrada(fila) {
-    for(let i=1; i<=intentos; i++)
+    for(let i=1; i<=limiteIntentos; i++)
         document.getElementById(`intento${fila}-letra${i}`).removeAttribute('disabled');
 }
 
