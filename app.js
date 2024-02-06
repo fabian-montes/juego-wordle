9a39af70ca51bdc94fe5546b766f4c680e98f0f7
Fabian Montes <fabianmontes11fab@gmail.com>
2024-02-05 22:42:37 -0600

    feat: habilitar entrada y estilo

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ let letras = 6;
 function crearEntradas() {
     let area = document.getElementById('entrada');
     area.innerHTML = '';
-    for(let i=0; i<intentos; i++) {
-        fila = `<div id="intento${i}">`;
-        for(let j=0; j<letras; j++) {
-            fila += `<input id="intento${i}-letra${j}" maxlength="1">`;
+    for(let i=1; i<=intentos; i++) {
+        fila = `<div id="intento${i}"> <span>intento #${i}<span>`;
+        for(let j=1; j<=letras; j++) {
+            fila += `<input class="casilla" id="intento${i}-letra${j}" maxlength="1" disabled>`;
         }
         fila += "</div";
         area.innerHTML += fila;
     }
 }
+function habilitarEntrada(fila) {
+    for(let i=1; i<=intentos; i++)
+        document.getElementById(`intento${fila}-letra${i}`).removeAttribute('disabled');
+}
 
 crearEntradas();
+habilitarEntrada(1);
