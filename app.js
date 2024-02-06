ad6b643b5def522b81c117bfdec2eab9e30ee6c9
Fabian Montes <fabianmontes11fab@gmail.com>
2024-02-05 23:03:45 -0600

    feat: generador de palabra aleatoria

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 let intentos = 6;
 let pistas = 2;
 let letras = 6;
+let palabraSecreta = generarPalabraAleatoria();
+console.log(palabraSecreta);
 
+function generarPalabraAleatoria() {
+    const banco = '"ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let palabra = '';
+    for(let i=0; i<letras; i++) {
+        palabra += banco.charAt(Math.floor(Math.random() * banco.length));
+    }
+    return palabra;
+}
 function crearEntradas() {
     let area = document.getElementById('entrada');
     area.innerHTML = '';
@@ -13,6 +23,7 @@ function crearEntradas() {
         fila += "</div";
         area.innerHTML += fila;
     }
+    habilitarEntrada(1);
 }
 function habilitarEntrada(fila) {
     for(let i=1; i<=intentos; i++)
@@ -20,4 +31,3 @@ function habilitarEntrada(fila) {
 }
 
 crearEntradas();
-habilitarEntrada(1);
